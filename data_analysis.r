105f0a1917d5930680c407d955a1b26a045b5b4e
mbanov <m.banov7@gmail.com>
2022-01-24 13:27:07 +0100

    data analysis

--- data_analysis.r
+++ data_analysis.r
@@ -3,6 +3,7 @@ rm(list=ls())
 setwd("C:/Users/mbano/Documents/Faks/Usluge zasnovane na lokaciji/r-svm-classification")
 
 library(DataExplorer)
+library(ggplot2)
 
 car <- read.csv("datasets/clean datasets/car.csv", header=TRUE)
 car2 <- read.csv("datasets/clean datasets/car2.csv", header=TRUE)
@@ -16,6 +17,10 @@ car <- rbind(car, car2)
 car <- rbind(car, car3)
 walk <- rbind(walk, walk2)
 bus <- rbind(bus, bus2)
+total <- rbind(car, walk)
+total <- rbind(total, bus)
+total <- na.omit(total)
+total <- total[, c("x","y", "z", "class")]
 
 par(mar=c(6,2,5,2))
 par(mfrow=c(1,3))
@@ -30,6 +35,9 @@ accel_z <- data.frame(car=car[, 'z'], walk=walk[,'z'], bus=bus[,'z'])
 boxplot(accel_x, horizontal=TRUE, main="Accel.x")
 boxplot(accel_y, horizontal=TRUE, main="Accel.y")
 boxplot(accel_z, horizontal=TRUE, main="Accel.z")
+ggplot(total, aes(x=class, y=x, group=class)) + geom_boxplot() + coord_flip()+ scale_y_continuous(trans = scales::pseudo_log_trans())
+ggplot(total, aes(x=class, y=y, group=class)) + geom_boxplot() + coord_flip()+ scale_y_continuous(trans = scales::pseudo_log_trans())
+ggplot(total, aes(x=class, y=z, group=class)) + geom_boxplot() + coord_flip()+ scale_y_continuous(trans = scales::pseudo_log_trans())
 
 x <- append(append(accel_x$car, accel_x$walk), accel_x$bus)
 plot(density(na.omit(x)), main="Accel.x")
@@ -39,9 +47,5 @@ z <- append(append(accel_z$car, accel_z$walk), accel_z$bus)
 plot(density(na.omit(z)), main="Accel.z")
 
 par(mfrow=c(1,1))
-total <- rbind(car, walk)
-total <- rbind(total, bus)
-total <- na.omit(total)
-total <- total[, c("x","y", "z", "class")]
 # total <- total[, c("x","y", "z")]
 plot_correlation(total)
